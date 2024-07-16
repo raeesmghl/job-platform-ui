a2b28f0e64f648a61cb9e0af78d7ec335fe9c7b8
Raees Mughal <raees.mughal.official@gmail.com>
2024-07-16 21:46:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let navLinks = document.querySelectorAll('.slide-nav .item');
 
+navLinks.forEach(item => {
+    item.addEventListener('click',()=>{
+        navLinks.forEach((link)=>{
+            link.classList.remove('active');
+        })  
+        item.classList.add('active')
+    })
+});
